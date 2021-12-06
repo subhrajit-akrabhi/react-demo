e8693ac9ce2eb28579106a6236b5895ca70f5e92
subhrajit-infoviz <subhrajit.pattnayak@infoviz.co>
2021-12-06 23:31:24 +0530

    content changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         <h1>Testing with github</h1>
+         <h1>Testing with github ci/cd</h1>
         </p>
         <a
           className="App-link"
